5e5e50edc75f5cf0f4c301f757943209827739c9
= <thomite@gmail.com>
2020-09-06 17:54:10 +0100

    Introduced idea of review schedule so reviews can go backwards.

--- bulk_reviewer.py
+++ bulk_reviewer.py
@@ -10,27 +10,38 @@ from PyQt5.QtWidgets import QHBoxLayout, QGroupBox, QDialog, QGridLayout, QLabel
 import glob
 import random
 import pandas as pd
+import numpy as np
 import argparse
 
-def initialise_review_dataset(directory):
+def initialise_review_dataset(directory, schedule_length):
 
-	fn = directory + "/reviews.csv"
+    fn = directory + "/reviews.csv"
 
-	if os.path.exists(fn):
-		print("Resuming previous review session.")
-		return pd.read_csv(fn)
+    if os.path.exists(fn):
+        print("Resuming previous review session.")
+        dset = pd.read_csv(fn)
 
-	else:
+        unreviewed = dset.query("reviewed == False")
+        if len(unreviewed) > 0:
+            schedule_index = unreviewed["schedule"].min()
+        else:
+            schedule_index = 0
+    else:
 
-		filenames = glob.glob(directory + "/*.*")
+        filenames = glob.glob(directory + "/*.*")
+        # TODO: keep only filenames with image extensions
 
-		dset = pd.DataFrame()
+        dset = pd.DataFrame()
 
-		dset["filename"] = filenames
-		dset["reviewed"] = False
-		dset["score"] = 0
+        dset["filename"] = filenames
+        dset["reviewed"] = False
+        dset["score"] = 0
 
-	return dset
+        num_schedule = int(np.ceil(len(dset) / schedule_length))
+        dset["schedule"] = np.array([[i for _ in range(schedule_length)] for i in range(num_schedule)]).flatten()[:len(dset)]
+        schedule_index = 0
+
+    return (dset, schedule_index)
 
 def get_unreviewed(dset, N=10):
 
@@ -43,214 +54,226 @@ def update_reviews(original_dset, updated_dset):
 
 class ClickableLabel(QLabel):
 
-	unselected_style = "QLabel {padding: 5px; background-color: #000000;}"
-	selected_style = "QLabel {padding: 5px; background-color: #C10000;}"
+    unselected_style = "QLabel {padding: 5px; background-color: #000000;}"
+    selected_style = "QLabel {padding: 5px; background-color: #C10000;}"
 
-	def __init__(self, ID, filename, parent):
-		super().__init__()
-		self.setAcceptDrops(True)
+    def __init__(self, ID, filename, parent):
+        super().__init__()
+        self.setAcceptDrops(True)
 
-		pixmap = QPixmap(filename)
-		pixmap = pixmap.scaled(parent.image_width, parent.image_height, QtCore.Qt.KeepAspectRatio)
+        pixmap = QPixmap(filename)
+        pixmap = pixmap.scaled(parent.image_width, parent.image_height, QtCore.Qt.KeepAspectRatio)
 
-		self.setPixmap(pixmap)
-		self.clicked = False
-		self.setStyleSheet(ClickableLabel.unselected_style)
+        self.setPixmap(pixmap)
+        self.clicked = False
+        self.setStyleSheet(ClickableLabel.unselected_style)
 
-		self.setAlignment(Qt.AlignCenter)
+        self.setAlignment(Qt.AlignCenter)
 
-		self.ID = ID 
-		self.filename = filename
-		self.mainwindow_parent = parent
+        self.ID = ID 
+        self.filename = filename
+        self.mainwindow_parent = parent
 
-	def toggle(self):
+    def toggle(self):
 
-		if self.clicked == False:
+        if self.clicked == False:
 
-			self.clicked = True
-			self.mainwindow_parent.set_review(self.ID, -1)
-			self.setStyleSheet(ClickableLabel.selected_style)
+            self.clicked = True
+            self.mainwindow_parent.set_review(self.ID, -1)
+            self.setStyleSheet(ClickableLabel.selected_style)
 
-		else:
-			self.clicked = False
-			self.mainwindow_parent.set_review(self.ID, 1)
-			self.setStyleSheet(ClickableLabel.unselected_style)
+        else:
+            self.clicked = False
+            self.mainwindow_parent.set_review(self.ID, 1)
+            self.setStyleSheet(ClickableLabel.unselected_style)
 
-	def mousePressEvent(self, event):
+    def mousePressEvent(self, event):
         
-		if event.buttons() & QtCore.Qt.LeftButton:
+        if event.buttons() & QtCore.Qt.LeftButton:
 
-			print(f"Clicked {self.ID}")
-			self.toggle()
+            print(f"Clicked {self.ID}")
+            self.toggle()
 
 
 class App(QMainWindow):
 
-	def __init__(self, parser_args):
-		super().__init__()
+    def __init__(self, parser_args):
+        super().__init__()
 
-		self.parser_args = parser_args
-		self.left = 50
-		self.top = 50
-		self.title = 'Bulk reviewer'
-		self.width = 960
-		self.height = 640
+        self.parser_args = parser_args
+        self.left = 50
+        self.top = 50
+        self.title = 'Bulk reviewer'
+        self.width = 960
+        self.height = 640
 
-		self.num_rows = self.parser_args.num_rows
-		self.num_cols = self.parser_args.num_cols
+        self.num_rows = self.parser_args.num_rows
+        self.num_cols = self.parser_args.num_cols
+        self.schedule_size = self.num_rows * self.num_cols
 
-		self.image_width = self.width / self.num_cols
-		self.image_height = self.height / self.num_rows
+        self.image_width = self.width / self.num_cols
+        self.image_height = self.height / self.num_rows
 
-		self.directory = self.parser_args.directory
-		self.reviews = initialise_review_dataset(self.directory)
+        self.directory = self.parser_args.directory
+        self.reviews, self.schedule_index = initialise_review_dataset(self.directory, self.schedule_size)
+        self.max_schedule_index = self.reviews["schedule"].max()
 
-		self.initUI()
+        self.initUI()
 
-	def initUI(self):
+    def initUI(self):
 
-		self.setWindowTitle(self.title)
-		self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
 
-		self.central_widget = QWidget()
-		self.central_widget_layout = QGridLayout()
-		self.central_widget.setLayout(self.central_widget_layout)
-		self.setCentralWidget(self.central_widget)
+        self.central_widget = QWidget()
+        self.central_widget_layout = QGridLayout()
+        self.central_widget.setLayout(self.central_widget_layout)
+        self.setCentralWidget(self.central_widget)
 
-		# -- Create stuff
-		self.create_montage_panel()
-		self.create_progress_panel()
+        # -- Create stuff
+        self.create_montage_panel()
+        self.create_progress_panel()
 
-		# -- Place stuff
-		self.central_widget_layout.addWidget(self.montage_panel, 0, 0)
-		self.central_widget_layout.addWidget(self.progress_panel, 1, 0)
+        # -- Place stuff
+        self.central_widget_layout.addWidget(self.montage_panel, 0, 0)
+        self.central_widget_layout.addWidget(self.progress_panel, 1, 0)
 
-		self.update_montage_panel()
-		self.update_progress_panel()
-		self.show()
+        self.update_montage_panel()
+        self.update_progress_panel()
+        self.show()
 
-	def create_montage_panel(self):
-		self.montage_panel = QWidget()
-		self.montage_panel_layout = QGridLayout()
-		self.montage_panel.setLayout(self.montage_panel_layout)
+    def create_montage_panel(self):
 
-	def create_progress_panel(self):
+        self.montage_panel = QWidget()
+        self.montage_panel_layout = QGridLayout()
+        self.montage_panel.setLayout(self.montage_panel_layout)
 
-		self.progress_panel = QWidget()
-		self.progress_panel_layout = QGridLayout()
-		self.progress_panel.setLayout(self.progress_panel_layout)
+    def create_progress_panel(self):
 
-		self.progress_bar = QProgressBar()
-		self.label_progress = QLabel(self.progress_panel)
-		#self.label_summary = QLabel(self.progress_panel)
+        self.progress_panel = QWidget()
+        self.progress_panel_layout = QGridLayout()
+        self.progress_panel.setLayout(self.progress_panel_layout)
 
-		self.progress_panel_layout.addWidget(self.progress_bar, 0, 0)
-		self.progress_panel_layout.addWidget(self.label_progress, 0, 1)
-		#self.progress_panel_layout.addWidget(self.label_summary, 1, 0)
+        self.progress_bar = QProgressBar()
+        self.label_progress = QLabel(self.progress_panel)
 
-	def clear_montage_panel(self):
-		for i in reversed(range(self.montage_panel_layout.count())): 
-			self.montage_panel_layout.itemAt(i).widget().deleteLater()
+        self.progress_panel_layout.addWidget(self.progress_bar, 0, 0)
+        self.progress_panel_layout.addWidget(self.label_progress, 0, 1)
 
-	def update_progress_panel(self):
+    def clear_montage_panel(self):
+        for i in reversed(range(self.montage_panel_layout.count())): 
+            self.montage_panel_layout.itemAt(i).widget().deleteLater()
 
-		num_reviewed = sum(self.reviews["reviewed"] == True)
-		num_remaining = sum(self.reviews["reviewed"] == False)
-		total = len(self.reviews)
-		percent_done = (num_reviewed / total) * 100
-		
-		num_flagged = sum(self.reviews["score"] == -1)
-		num_okay = sum(self.reviews["score"] == 1)
+    def update_progress_panel(self):
 
-		description = f"({num_reviewed} of {total}) [{num_flagged} flagged]"
+        num_reviewed = sum(self.reviews["reviewed"] == True)
+        num_remaining = sum(self.reviews["reviewed"] == False)
+        total = len(self.reviews)
+        percent_done = (num_reviewed / total) * 100
+        
+        num_flagged = sum(self.reviews["score"] == -1)
+        num_okay = sum(self.reviews["score"] == 1)
 
-		self.progress_bar.setValue(percent_done)
-		self.label_progress.setText(description)
-		#self.label_summary.setText(summary)
+        description = f"({num_reviewed} of {total}) [{num_flagged} flagged]"
 
-	def update_montage_panel(self):
-		
-		self.clear_montage_panel()
+        self.progress_bar.setValue(percent_done)
+        self.label_progress.setText(description)
 
-		N = int(self.num_rows * self.num_cols)
+    def update_montage_panel(self):
 
-		self.currently_showing_images = get_unreviewed(self.reviews, N=N)
+        # TODO: fix slice error 
+        self.currently_showing_images = self.reviews.loc[self.reviews["schedule"] == self.schedule_index]
 
-		n = 0
+        n = 0
+        for r in range(self.num_rows):
+            for c in range(self.num_cols):
 
-		for r in range(self.num_rows):
-			for c in range(self.num_cols):
+                if n < len(self.currently_showing_images):
+                    index = self.currently_showing_images.index[n]
+                    filename = self.currently_showing_images.loc[index, "filename"]
+                    l = ClickableLabel(index, filename, self)
 
-				if n < len(self.currently_showing_images):
-					index = self.currently_showing_images.index[n]
-					filename = self.currently_showing_images.loc[index, "filename"]
-					l = ClickableLabel(index, filename, self)
-					self.montage_panel_layout.addWidget(l, r, c)
-					n += 1
+                    # Flagged images now load as toggled/red
+                    if self.currently_showing_images.loc[index, "score"] == -1:
+                        l.toggle()
+                    self.montage_panel_layout.addWidget(l, r, c)
+                    n += 1
 
+    def set_review(self, index, score):
+        
+        
+        self.currently_showing_images.loc[index, "reviewed"] = True
+        self.currently_showing_images.loc[index, "score"] = score
+       
+    def commit_reviews(self):
 
-	def set_review(self, index, score):
-		
-		self.currently_showing_images.loc[index, "reviewed"] = True
-		self.currently_showing_images.loc[index, "score"] = score
+        remaining = self.currently_showing_images["reviewed"] == False
+        self.currently_showing_images.loc[remaining, "reviewed"] = True
+        self.currently_showing_images.loc[remaining, "score"] = 1
 
-	def commit_reviews(self):
+        update_reviews(self.reviews, self.currently_showing_images)
 
-		remaining = self.currently_showing_images["reviewed"] == False
-		self.currently_showing_images.loc[remaining, "reviewed"] = True
-		self.currently_showing_images.loc[remaining, "score"] = 1
+    def next(self):
 
-		update_reviews(self.reviews, self.currently_showing_images)
+        self.commit_reviews()
 
-	def next(self):
+        self.schedule_index += 1
+        if self.schedule_index > self.max_schedule_index:
+            self.schedule_index = 0
 
-		self.commit_reviews()
-		self.update_montage_panel()
-		self.update_progress_panel()
+        self.update_montage_panel()
+        self.update_progress_panel()
 
-	def previous(self):
+    def previous(self):
 
-		print("previous() not implemented yet")
+        self.commit_reviews()
 
-	def keyPressEvent(self, event):
+        self.schedule_index -= 1
+        if self.schedule_index < 0:
+            self.schedule_index = self.max_schedule_index
 
-		key = event.key()
+        self.update_montage_panel()
+        self.update_progress_panel()
 
-		if key == Qt.Key_Escape or key == Qt.Key_Q:
-			self.close_window()
+    def keyPressEvent(self, event):
 
-		elif key == Qt.Key_Right or key == Qt.Key_D:
-			self.next()
+        key = event.key()
 
-		elif key == Qt.Key_Left or key == Qt.Key_A:
-			self.previous()
+        if key == Qt.Key_Escape or key == Qt.Key_Q:
+            self.close_window()
 
-		elif key == Qt.Key_P:
-			self.print_reviews()
+        elif key == Qt.Key_Right or key == Qt.Key_D:
+            self.next()
 
-	def print_reviews(self):
+        elif key == Qt.Key_Left or key == Qt.Key_A:
+            self.previous()
 
-		try:
-			self.reviews.to_csv(self.directory + "/reviews.csv", index=False)
+        elif key == Qt.Key_P:
+            self.print_reviews()
 
-		except:
-			print("Couldn't print to reviews.csv")
+    def print_reviews(self):
 
-	def close_window(self):
+        try:
+            self.reviews.to_csv(self.directory + "/reviews.csv", index=False)
 
-		self.close()
+        except:
+            print("Couldn't print to reviews.csv")
 
-	def closeEvent(self, event):
-		self.print_reviews()
-		self.deleteLater()
+    def close_window(self):
+
+        self.close()
+
+    def closeEvent(self, event):
+        self.print_reviews()
+        self.deleteLater()
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser(description='Bulk review images in a directory.')
-	parser.add_argument('directory')
-	parser.add_argument('--num_rows', type=int, default=3)
-	parser.add_argument('--num_cols', type=int, default=6)
+    parser = argparse.ArgumentParser(description='Bulk review images in a directory.')
+    parser.add_argument('directory')
+    parser.add_argument('--num_rows', type=int, default=3)
+    parser.add_argument('--num_cols', type=int, default=6)
 
-	app = QApplication(sys.argv)
-	ex = App(parser.parse_args())
-	sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    ex = App(parser.parse_args())
+    sys.exit(app.exec_())
